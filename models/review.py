d30310bf9fd9a8dfed2baf41fcd6d0f1692099f1
Samuel Anozie <samuelanozie10@yahoo.com>
2022-08-15 16:46:31 +0100

    fix console

--- models/review.py
+++ models/review.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
-"""
-Defines review class
-"""
-from models.base_model import BaseModel
+""" Review module for the HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from models import storage_type
+from sqlalchemy.orm import relationship
 
 
-class Review(BaseModel):
-    """Reviews made by users about a place"""
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Review classto store review information """
+
+    if storage_type == 'db':
+        __tablename__ = 'reviews'
+
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
+
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
--- models/user.py
+++ models/user.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
-"""
-User creation class
-"""
-from models.base_model import BaseModel
+"""This module defines a class User"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
 
 
-class User(BaseModel):
-    """Defines attributes for user creation"""
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+class User(BaseModel, Base):
+    """This class defines a user by various attributes"""
+    if models.storage_type == 'db':
+        __tablename__ = "users"
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user", cascade='all')
+        reviews = relationship("Review", backref="user", cascade='all')
+    else:
+        email = ''
+        password = ''
+        first_name = ''
+        last_name = ''
